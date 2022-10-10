5591c2a773a29ce1f81ec3e1be1141a7721abc7e
zisakmc <mishaela98@gmail.com>
2022-10-11 00:43:59 +0530

    new file

---
+++ sort/merge_sort.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int array[]={9,8,7,6,5,3,4,2,1};
+	int n = sizeof(array)/sizeof(int);
+	cout<<"before sort"<<"\n";
+	for(int i=0;i<n;i++){
+		cout<<" "<<array[i];
+	}
+
+	int mid = n/2;
+
+	return 0;
+}
